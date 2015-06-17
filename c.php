b708a38b5e4a7e0bdf262f371663a475647b8989
lkchoi <lester.choi@pith-studio.com>
2015-06-17 12:28:26 -0400

    Add utility functions, for random string generation

--- c.php
+++ c.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/utils.php';
+
 function upload()
 {
 	file_put_contents(__DIR__."/twitto.log", print_r($_FILES, true));
--- sort.php
+++ sort.php
@@ -1,3 +1,4 @@
+<?php require_once __DIR__ . '/utils.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
