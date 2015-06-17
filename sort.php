ed2d4e11da9348b8db9dd796dace33580cfb00b3
lkchoi <lester.choi@pith-studio.com>
2015-06-17 12:32:24 -0400

    Set the php js object to help send the portfolio token via x-editable POST

--- sort.php
+++ sort.php
@@ -88,6 +88,10 @@
 
     </div><!-- /.container -->
 
+    <script>
+      var php = { portfolio_token: '<?php echo $token ?>' };
+    </script>
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
